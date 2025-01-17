b66bd9e072f99e95e10725e5eb042e20c1096d82
jnachoo <80649974+jnachoo@users.noreply.github.com>
2025-01-17 10:13:51 -0300

    cambio de import

--- api.py
+++ api.py
@@ -5,7 +5,6 @@ import os
 from databases import Database
 from datetime import datetime
 import asyncio 
-import api_get as g
 
 
 # Obtén la URL de la base de datos desde las variables de entorno
@@ -53,6 +52,10 @@ async def shutdown():
     await database.disconnect()
 
 # Endpoints API 
+
+#------------------------------------------
+# -----------------BLS---------------------
+#------------------------------------------
 
 @app.get("/bls")
 async def ver_bls():
@@ -164,8 +167,8 @@ async def ver_bls_id(code:str):
         raise HTTPException(status_code=404, detail="Code de bl no encontrado")
     return result
 
-@app.get("/bls/naviera/{code}")
-async def ver_bls_id(code:str):
+@app.get("/bls/naviera/{nombre}")
+async def ver_bls_id(nombre:str):
     query = """                
                 select b.id,b.code as bl_code, e.nombre  as etapa, n.nombre  as naviera ,sb.descripcion_status as status, 
                 TO_CHAR(b.fecha, 'YYYY-MM-DD') as fecha ,TO_CHAR(b.proxima_revision, 'YYYY-MM-DD') as fecha_proxima_revision   
@@ -173,10 +176,11 @@ async def ver_bls_id(code:str):
                 join etapa e on e.id =b.id_etapa
                 join navieras n on n.id =b.id_naviera
                 join status_bl sb on b.id_status = sb.id
-                where n.nombre like :code;
+                where n.nombre like :nombre;
             """
-    code = f"{code}%"
-    result = await database.fetch_all(query=query, values={"code": code})
+    nombre = nombre.upper()
+    nombre = f"{nombre}%"
+    result = await database.fetch_all(query=query, values={"nombre": nombre})
     
     # Si no se encuentra la naviera, devolver el listado completo con un mensaje
     if not result:
@@ -195,6 +199,7 @@ async def ver_bls_id(etapa:str):
                 join status_bl sb on b.id_status = sb.id
                 where e.nombre like :etapa;
             """
+    etapa = etapa.upper()
     etapa = f"{etapa}%"
     result = await database.fetch_all(query=query, values={"etapa": etapa})
     
@@ -203,6 +208,12 @@ async def ver_bls_id(etapa:str):
         ver_info()
         raise HTTPException(status_code=404, detail="Etapa de bl no encontrado")
     return result
+
+
+
+#------------------------------------------
+# ---------------REQUESTS------------------
+#------------------------------------------
 
 @app.get("/requests")
 async def requests():
@@ -222,9 +233,8 @@ async def requests():
         raise HTTPException(status_code=404, detail="La ruta no existe")
     return result
 
-# 2. Ver requests
-@app.get("/requests/{bl_id}")
-async def requests_bl_id(bl_id: int):
+@app.get("/requests/{id_bl}")
+async def requests_id_bl(id_bl: int):
     query = """
             select r.id as id_request,h.id as id_html, b.code as bl_code,s.descripcion_status , r.mensaje,rr.descripcion as respuesta_request,
             b.fecha as fecha_bl, r.fecha as fecha_request   
@@ -233,104 +243,33 @@ async def requests_bl_id(bl_id: int):
             join respuesta_requests rr on rr.id = r.id_respuesta 
             join bls b on b.id = r.id_bl
             join status_bl s on s.id = b.id_status 
-            where b.id = :id;
+            where b.id = :id_bl;
             """
-    result = await database.fetch_all(query=query, values={"id": bl_id})
+    result = await database.fetch_all(query=query, values={"id_bl": id_bl})
     if not result:
         ver_info()
         raise HTTPException(status_code=404, detail="ID de bl no encontrado")
     return result
 
-# 3. Ver container por code
-@app.get("/container/code/{container_codigo}")
-async def container_code(container_codigo: str):
-    query = "SELECT * FROM containers WHERE code = :texto;"
-    result = await database.fetch_one(query=query, values={"texto": container_codigo})
-    if not result:
-        raise HTTPException(status_code=404, detail="Codigo de container no encontrado")
-    return result
-
-# 4. Ver container por bl_id
-@app.get("/container/bl_id/{bl_id}")
-async def container_bl_id(bl_id: int):
-    query = "SELECT * FROM containers WHERE bl_id = :id;"
-    result = await database.fetch_one(query=query, values={"id": bl_id})
-    if not result:
-        raise HTTPException(status_code=404, detail="ID de bl no encontrado")
-    return result
-
-# 5. Ver navieras
-@app.get("/navieras/{naviera_id}")
-async def navieras_todas(naviera_id: int):
-    # Consulta para buscar la naviera por ID
-    query = "SELECT id, nombre FROM navieras WHERE id = :naviera_id;"
-    result = await database.fetch_all(query=query, values={"naviera_id": naviera_id})
-    
-    # Si no se encuentra la naviera, devolver el listado completo con un mensaje
-    if not result:
-        query = "SELECT id, nombre FROM navieras order by id;"
-        result = await database.fetch_all(query=query, values={})
-        return {
-            "mensaje": "La naviera con el ID proporcionado no fue encontrada. El listado completo de navieras es:",
-            "navieras": result
-        }
-    
-    # Si se encuentra la naviera, devolver el resultado
-    return result
-
-# 6. Ver expo / impo
-@app.get("/navieras/{naviera_id}/{impo_expo}")
-async def navieras_impo_expo(impo_expo: str, naviera_id: int):
-    # Validación del id
-    if naviera_id>13 and naviera_id<0:
-        raise HTTPException(status_code=400, detail="ID de naviera no válido")
-
-    # Determinar etapa (1 para 'expo', 2 para 'impo')
-    etapa = 1 if impo_expo.lower() == "expo" else 2 if impo_expo.lower() == "impo" else None
-    if etapa is None:
-        raise HTTPException(status_code=400, detail="Código debe ser 'expo' para exportación o 'impo' para importación")
-
-    # Obtener el nombre de la naviera correspondiente al ID
-    #naviera_nombre = navieras[id]
-
-    # Consulta SQL
-    query = """ SELECT 'La naviera ' || n.nombre || ' tiene ' || COUNT(b.id_etapa) || ' exportaciones' AS mensaje
-        FROM bls b join navieras n ON b.id_naviera = n.id WHERE b.id_etapa = :etapa and b.id_naviera = :naviera_id GROUP BY n.nombre;""" if impo_expo.lower() =="expo" else """
-        SELECT 'La naviera ' || n.nombre || ' tiene ' || COUNT(b.id_etapa) || ' importaciones' AS mensaje
-        FROM bls b join navieras n ON b.naviera_id = n.id WHERE b.etapa = :etapa and b.naviera_id = :naviera_id GROUP BY n.nombre""" 
-
-    # Ejecución de la consulta
-    result = await database.fetch_one(query=query, values={ "etapa": etapa, "naviera_id": naviera_id})
-    if not result:
-        raise HTTPException(status_code=404, detail="Item no encontrado")
-
-    return result
-
-
-#@app.get("/naviera/{naviera_id}")
-async def naviera(naviera_id: int):
-    # Validación del ID de naviera
-    if naviera_id > 13 or naviera_id < 1:
-        raise HTTPException(status_code=400, detail="ID de naviera no válido, ingresar entre 1 y 13")
-
-    # Consulta SQL para contar exportaciones e importaciones
+@app.get("/requests/{code}")
+async def requests_code(code: str):
     query = """
-        SELECT 'La naviera ' || n.nombre || ' tiene ' || 
-               SUM(CASE WHEN b.id_etapa = 1 THEN 1 ELSE 0 END) || ' exportaciones y ' || 
-               SUM(CASE WHEN b.id_etapa = 2 THEN 1 ELSE 0 END) || ' importaciones' AS mensaje
-        FROM bls b
-        JOIN navieras n ON b.id_naviera = n.id
-        WHERE b.id_naviera = :naviera_id
-        GROUP BY n.nombre;
-    """
-
-    # Ejecución de la consulta
-    result = await database.fetch_one(query=query, values={"naviera_id": naviera_id})
+            select r.id as id_request,h.id as id_html, b.code as bl_code,s.descripcion_status , r.mensaje,rr.descripcion as respuesta_request,
+            b.fecha as fecha_bl, r.fecha as fecha_request   
+            from requests r
+            join html_descargados_temp h on r.id_html = h.id
+            join respuesta_requests rr on rr.id = r.id_respuesta 
+            join bls b on b.id = r.id_bl
+            join status_bl s on s.id = b.id_status 
+            where b.code = :code;
+            """
+    code = f"{code}%"
+    result = await database.fetch_all(query=query, values={"code": code})
     if not result:
         ver_info()
-        raise HTTPException(status_code=404, detail="Naviera no encontrada")
-
+        raise HTTPException(status_code=404, detail="Códdigo de bl no encontrado")
     return result
+
 
 # 4. Ver informacion
 @app.get("/info")
@@ -343,7 +282,7 @@ def ver_info():
         "1.3":"/bls/code/escribir_code",
         "2.":"Estas son las rutas relacionadas a requests",
         "2.0":"/requests",
-        "3.":"/naviera/escrbir_id_naviera"
+        "2.1":"/request",
     }
     return mensaje
 
